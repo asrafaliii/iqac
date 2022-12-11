5da496647d23fb2574ac6f937118913d826b290e
unknown <moha.asrafali@gmail.com>
2022-12-11 16:21:39 +0600

    added react swiper

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import "./Hero.css";
 import banner from "../../assets/banner.jpg";
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
 const Hero = () => {
     return (
         <div className='hero-container'>
@@ -12,7 +19,30 @@ const Hero = () => {
                 <a href='#' className='button'>learn more</a>
             </div>
             <div className='hero-img'>
+                <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={10}
+                slidesPerView={1}
+                navigation
+                breakpoints={{
+                    
+                }}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log("slide change")}
+                >
+                <SwiperSlide>
                 <img src={banner} />
+                </SwiperSlide>
+                <SwiperSlide>
+                <img src={banner} />
+                </SwiperSlide>
+                <SwiperSlide>
+                <img src={banner} />
+                </SwiperSlide>
+                </Swiper>
             </div>
         </div>
     );
