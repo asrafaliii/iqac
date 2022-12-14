5e83b01c69032f408476285c58aea934a682f968
unknown <moha.asrafali@gmail.com>
2022-12-14 11:39:15 +0600

    nav responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,48 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png"
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
   return (
-    <nav className="navbar">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/team">Team</a>
-      <a href="/activity">Activity</a>
-      <a href="/documents">Documents</a>
-      <a href="/trainingworkshop">Traning & Workshop</a>
-      <a href="/newsevent">News & Event</a>
-      <a href="/contactus">Contact Us</a>
-    </nav>
+    <header>
+			<img className="logo" src={logo} />
+			<nav ref={navRef}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/team">Team</Link>
+      <Link to="/activity">Activity</Link>
+      <Link to="/documents">Documents</Link>
+      <Link to="/trainingworkshop">Traning & Workshop</Link>
+      <Link to="/newsevent">News & Event</Link>
+      <Link to="/contactus">Contact Us</Link>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button className="nav-btn" onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+    // <nav className="navbar">
+      // <Link to="/">Home</Link>
+      // <Link to="/about">About</Link>
+      // <Link to="/team">Team</Link>
+      // <Link to="/activity">Activity</Link>
+      // <Link to="/documents">Documents</Link>
+      // <Link to="/trainingworkshop">Traning & Workshop</Link>
+      // <Link to="/newsevent">News & Event</Link>
+      // <Link to="/contactus">Contact Us</Link>
+    // </nav>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
     <Hero />
     <Card />
     <Activities />
+   
   </div>;
 };
 
